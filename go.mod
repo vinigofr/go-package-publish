0d05b232a16f1689e13509189a5dd04a3dc8df10
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-09 16:07:43 -0300

    Envia dados

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vinigofr/go-package-publish
+
+go 1.18
---
+++ vinicius/vinicius.go
@@ -0,0 +1,9 @@
+// Paco teste para a documentacão
+package vinicius
+
+import "fmt"
+
+// Vinicius imprime um belo recado
+func Vinicius() {
+	fmt.Println("Olá, eu sou o ViniG")
+}
